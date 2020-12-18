93b58aa8b95e207d0e9e4ca4f5cfcfd710e152b1
Sonny Li <sonnynomnom@gmail.com>
2020-12-18 10:58:11 -0500

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-# Mentor Bot
-
-import openai
-openai.api_key = "sk-MICsuHUrAH9yEUE1JPfkttn0dNTIGZwSsVh604Fq"
-
-response = openai.Completion.create(engine="davinci", prompt="This is a test", max_tokens=5)
