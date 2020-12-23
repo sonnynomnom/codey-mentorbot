6c9dd20680f1099491a1bbb2b8747fdc2cb80946
Sonny Li <sonnynomnom@gmail.com>
2020-12-23 16:21:48 -0500

    update

--- app/main.py
+++ app/main.py
@@ -31,8 +31,8 @@ cs_chatbot = ChatBot(
 )
 
  # Training with Personal Ques & Ans 
-training_data_simple = open('./training_data/normal.txt').read().splitlines()
-training_data_personal = open('./training_data/all.txt').read().splitlines()
+training_data_simple = open('https://raw.githubusercontent.com/sonnynomnom/codey-mentorbot/main/app/training_data/normal.txt').read().splitlines()
+training_data_personal = open('https://raw.githubusercontent.com/sonnynomnom/codey-mentorbot/main/app/training_data/all.txt').read().splitlines()
 
 training_data = training_data_simple + training_data_personal
 
